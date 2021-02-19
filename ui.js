6f8723e7e524b590829a67acbf06b1420f3ef17a
oliver <soliz78120@gmail.com>
2021-02-19 15:44:32 -0400

    UI terminada

--- ui.js
+++ ui.js
@@ -14,6 +14,17 @@ class Ui{
         this.manager.addProducts(p1);
         this.manager.addProducts(p2);
         this.manager.addProducts(p3);
+        this.loadEvents();
+    }
+    loadEvents(){
+        this.uiForm.addEventListener("submit", (e)=>{
+            e.preventDefault();
+            this.addProducts(
+                this.uiName.value,
+                this.uiDescription.value,
+                this.uiQuantity.value
+            );
+        })
     }
     loadTable(){
         var html = "";
@@ -26,6 +37,11 @@ class Ui{
             </tr>`;
         }
         this.container.innerHTML = html ;
+    }
+    addProducts(name, description, quantity){
+        let p1 = new Products(name, description, quantity);
+        this.manager.addProducts(p1);
+        this.loadTable();
     }
 }
 let ui = new Ui();
